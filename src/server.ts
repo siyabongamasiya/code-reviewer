38f0a2c21dc0313ae6e736b1faad8ef6c6728980
siyabonga <siyabongakhanyile76@gmail.com>
2025-11-06 14:59:37 +0200

    mod:changed type folder to model.added sockets folder and file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
-import express from "express";
+import express,{Express,Response,Request} from "express";
 import dotenv from "dotenv";
 import { testConnection } from "./config/database";
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app : Express= express();
+const PORT = process.env.PORT || 4040;
 
 app.use(express.json());
+
+app.get('/', (req : Request, res : Response) => {
+  res.send('Welcome to the code collaboration review platform!')
+})
 
 
 const startServer = async () => {
@@ -16,6 +20,5 @@ const startServer = async () => {
     console.log(`Server is listening on port http://localhost:${PORT}`);
   });
 };
-
 
 startServer()
